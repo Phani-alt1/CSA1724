8e60b7b6a39d16093b233cfd37fb0e87e9359ab3
Phani-alt1 <75060953+Phani-alt1@users.noreply.github.com>
2025-06-24 14:24:32 +0530

    dfs

---
+++ 8th.py
@@ -0,0 +1,22 @@
+def dfs(graph,start):
+    visited=set()
+    stack=[start]
+    result=[]
+
+    while stack:
+        node=stack.pop()
+        if node not in visited:
+            result.append(node)
+            visited.add(node)
+            stack.extend(reversed(graph[node]))
+    return result
+
+graph ={
+    'A': ['B','C'],
+    'B': ['D','E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+print("DFS: ",dfs(graph,'A'))
